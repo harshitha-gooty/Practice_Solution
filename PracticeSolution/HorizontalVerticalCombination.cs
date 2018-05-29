7d216427f9fdfcf0ac412704ac9685a18b63c32c
Harshitha Gooty <harshitha.gooty@hpe.com>
2018-05-29 11:55:05 +0530

    Initial commit

---
+++ PracticeSolution/HorizontalVerticalCombination.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSolution
+{
+    public class HorizontalVerticalCombination
+    {
+        public List<string> DisplayVerticalCombination(string alpha)
+        {
+            //string alpha = "abcdef";
+            List<string> result = new List<string>();
+            for (int i = 0; i < alpha.Length; i++)
+            {
+                result.Add(alpha.Substring(i, 1));
+            }
+            for (int y = 2; y < alpha.Length; y++)
+            {
+                int z = (alpha.Length / y);
+                int index = 0;
+                while (z > 0)
+                {
+                    result.Add(alpha.Substring(index, y));
+                    index += y;
+                    z--;
+                }
+
+            }
+            return result;
+        }
+
+        //public List<string> DisplayVerticalCombination(string a)
+        //{
+        //    char[] alpha = new char[] { 'a', 'b', 'c', 'd', 'e', 'f' };
+        //    List<string> result = new List<string>();
+        //    for (int i = 0; i < alpha.Count(); i++)
+        //    {
+        //        result.Add(alpha.ToString());
+        //    }
+        //    for (int y = 2; y < alpha.Count(); y++)
+        //    {
+        //        string str = new string(alpha);
+        //    }
+        //    return result;
+        //}
+    }
+}
+
+
---
+++ PracticeSolution/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSolution
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Occurances of a character
+            string re = "abcdaf";
+            int count1 = re.Split('a').Length - 1;
+            int count = re.ToArray().Count(x => x == 'a');
+            Console.WriteLine(count);
+            #endregion
+
+            #region Horizntal vertical combination
+            Console.WriteLine("Enter the string");
+            string input = Console.ReadLine();
+            //HorizontalVerticalCombination hvc = new HorizontalVerticalCombination();
+            //List<string> result = hvc.DisplayVerticalCombination(input);
+            //foreach (var item in result)
+            //{
+            //  Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region Prime or not
+            Console.WriteLine("enter the number");
+            string number = Console.ReadLine();
+            Methods methods = new Methods();
+            Console.WriteLine(methods.PrimeOrNot(number));
+            #endregion
+        }
+
+    }
+
+    public class Methods
+    {
+        public string PrimeOrNot(string number)
+        {
+            string result = string.Empty;
+            int no = Convert.ToInt32(number);
+            int k = 0;
+            for (int i = 2; i < no / 2; i++)
+            {
+                if (no % i == 0)
+                {
+                    k++;
+                }
+            }
+            if (k == 2)
+            {
+                result = "prime number";
+            }
+            else
+                result = "not prime number";
+            return result;
+
+        }
+    }
+}
